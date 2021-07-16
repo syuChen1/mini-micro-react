39ed4ff66ad1c1036a1133b350bc2dd61472f637
Siyu Chen <siyuchen1@ufl.edu>
2021-07-16 06:34:34 -0400

    screen for posts

---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title,
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group my-3'>
+          <label>Title</label>
+          <input
+            value={title}
+            className='form-control'
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
