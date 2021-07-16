f6a6da667db2c97025d76b4bfee0ce2416012850
Siyu Chen <siyuchen1@ufl.edu>
2021-07-16 07:32:22 -0400

    display comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -20,15 +22,17 @@ const PostList = () => {
         style={{ width: '30%', marginBottom: '20px' }}
         key={post.id}
       >
-        <div className='card-body'>
+        <div className='card-body '>
           <h3>{post.title}</h3>
         </div>
+        <CommentList postId={post.id} />
+        <CommentCreate postId={post.id} />
       </div>
     );
   });
 
   return (
-    <div className='d-flex flex-row flex-wrap justify-content-bewteen'>
+    <div className='d-flex flex-row flex-wrap justify-content-between'>
       {renderedPosts}
     </div>
   );
